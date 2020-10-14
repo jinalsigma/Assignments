1c8d9be7c5663364f91cf64ef5aa866414257d29
jinalsigma <jinal.shah@sigmainfo.net>
2020-10-14 10:29:47 +0530

    Assignment completed with the image produced

---
+++ Assignment_2/index.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jinalshah
+ * Date: 13/10/20
+ * Time: 6:47 PM
+ */
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Calculator</title>
+</head>
+<body>
+<p><div class="main">
+    <div class="container">
+    <div class="container2">
+        <input type="text" placeholder="Calculator">
+        <button class="black">AC</button>
+        <button class="black">+/-</button>
+        <button class="black">&#8730;</button>
+        <button class="black">/</button>
+        <button class="white">7</button>
+        <button class="white">8</button>
+        <button class="white">9</button>
+        <button class="black">X</button>
+        <button class="white">4</button>
+        <button class="white">5</button>
+        <button class="white">6</button>
+        <button class="black">-</button>
+        <button class="white">1</button>
+        <button class="white">2</button>
+        <button class="white">3</button>
+        <button class="black">+</button>
+        <button class="white">0</button>
+        <button class="white">.</button>
+        <button class="white">%</button>
+        <button class="blue">=</button>
+        
+    </div>
+    </div>
+</div></p>
+</body>
+</html>
