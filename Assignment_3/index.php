d8042e47dd9ffbeb81d2bcb3a3c4a6c3077626df
jinalsigma <jinal.shah@sigmainfo.net>
2020-10-14 13:14:52 +0530

    Hover dropdown

---
+++ Assignment_3/index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jinalshah
+ * Date: 14/10/20
+ * Time: 10:41 AM
+ */
+?>
+<html>
+<head>
+    <title>Menu</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="main">
+        <a href="#">Home</a>
+        <a href="#">About us</a>
+        <div class="container">
+            <a class="dropbtn">Potfolio</a>
+            <div class="dropdown">
+                <a href="#">Page 1</a>
+                <a href="#">Page 2</a>
+                <a href="#">Page 3</a>
+                <a href="#">Page 4</a>
+            </div>
+        </div>
+        <a href="#">Contact</a>
+    </div>
+</body>
+</html>
